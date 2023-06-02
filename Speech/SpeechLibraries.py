42050740f26e23fa5f073aa70148cfde54301b35
qiblatainf <qiblatain@live.com>
2023-06-02 14:48:14 +0500

    Speech Testing Component Functional

--- Speech/SpeechLibraries.py
+++ Speech/SpeechLibraries.py
@@ -4,9 +4,13 @@ class SpeechLibrary:
         self.test_audio = test_audio
           
     def lib(self): 
-        if (self.module_name == "google_transcribe"):
+        if (self.module_name == "google_transcribe" and self.test_audio != "large"):
             from Speech.SpeechToTextLibraries.GoogleTranscribe.google_transcribe import transcribe
             return transcribe(self.test_audio) 
+        
+        if (self.test_audio == "large"):
+            print("Audio files greater than 2 minutes not supported.")
+            
         
         #Add other libraries here
         
